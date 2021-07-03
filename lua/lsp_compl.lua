1bfe8f8484bc50fa0792657408486e4990dff6df
Mathias Fussenegger <f.mathias@zignar.net>
2021-07-03 17:16:04 +0200

    Include `:` in boundary pattern

--- lua/lsp_compl.lua
+++ lua/lsp_compl.lua
@@ -122,7 +122,7 @@ function M.find_start(line, cursor_pos)
   local line_to_cursor = line:sub(1, cursor_pos)
   local idx = 0
   while true do
-    local i = string.find(line_to_cursor, '[^a-zA-Z0-9_]', idx + 1)
+    local i = string.find(line_to_cursor, '[^a-zA-Z0-9_:]', idx + 1)
     if i == nil then
       break
     else
